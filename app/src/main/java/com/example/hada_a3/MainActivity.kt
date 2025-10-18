e23a3cba4867ced6cebc0d2eb9bb3bdbced5b7df
Chisora <chisorah15092004@gmail.com>
2025-10-18 15:48:54 +1300

    Basic layout for main menu done

--- app/src/main/java/com/example/hada_a3/MainActivity.kt
+++ app/src/main/java/com/example/hada_a3/MainActivity.kt
@@ -3,15 +3,26 @@ package com.example.hada_a3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.hada_a3.ui.theme.Hada_A3Theme
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Hada_A3Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "GAMES APP",
+                    UI(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,24 +40,75 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+//Global list of names of the thumbnails
+val gameList = listOf(
+    "word",
+    "bird",
+    "tic"
+)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TitlePage(modifier: Modifier = Modifier) {
+    Column{
+        //Display title
+        Box(
+            modifier = Modifier
+                .border(2.dp, Color.White, shape = androidx.compose.foundation.shape.RoundedCornerShape(10.dp))
+                .fillMaxWidth()
+                .padding(10.dp)
+        ){
+            Text(
+                "Games App",
+                modifier = Modifier
+                    .padding(10.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        //Use a lazy vertical grid to show thumbnails of each game
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(1),
+            contentPadding = PaddingValues(10.dp),
+            modifier = modifier.fillMaxSize()
+        ) {
+            items(gameList.size){ index ->
+                val name = gameList[index]
+                //Show the thumbnail of each game
+                Box(
+                    modifier = Modifier
+                        .border(2.dp, Color.White, shape = androidx.compose.foundation.shape.RoundedCornerShape(10.dp))
+                        .fillMaxWidth()
+                        .padding(10.dp)
+                ){
+                    Text(
+                        name,
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .align(Alignment.Center)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun UI(modifier: Modifier = Modifier){
+    //navController to navigate between main screen and games
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "MainMenu", builder = {
+        //Define the main menu composable
+        composable("MainMenu"){ TitlePage() }
+        //Define the each game's composable
+//        composable("WordGuessGame") { WordGuessGame() }
+//        composable("BirdGame") { BirdGame() }
+//        composable("TicTacToe") { TicTacToe() }
+    })
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TitlePreview() {
     Hada_A3Theme {
-        Greeting("Android")
+        UI()
     }
 }
-
-
-//Start with a title page with horizontal thumbnails for each game
-    //Wordle clone
-    //Flappy bird clone
-    //Tic-Tac-Toe
